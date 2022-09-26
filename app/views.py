2249a0539911a8af8caaedaff06adc7bea1947a9
Neel Dandiwala <neeldandi@gmail.com>
2022-09-27 02:18:49 +0530

    minor changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from twilio.twiml.messaging_response import MessagingResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-# Create your views here.
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        incoming_message = request.POST['Body'].lower()
+        
+        twilio_response = MessagingResponse()
+        actual_message = twilio_response.message()
+        
+        if incoming_message == 'Hello':
+            response = "Hey How are you"
+            actual_message.body(response)
+            
+        return HttpResponse(str(twilio_response))
+        
